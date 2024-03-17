75db2ca80fd0383747b1c674a1e38669fc984b30
Abel Santillan Rodriguez <diversia.abel@gmail.com>
2024-03-16 22:00:37 -0600

    Add new files and modules for Adaptive Fourier Neural Operator

---
+++ fno/__init__.py
@@ -0,0 +1,5 @@
+from .afnonet import AdaptiveFourierNeuralOperator as AFNO
+from .afno import FourierBlock
+from .patch_embedding import PatchEmbedding
+from .mlp import MLP
+from .spectral_convolution import AdaptiveSpectralConvolution
