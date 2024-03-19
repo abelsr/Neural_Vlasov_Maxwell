7106ca3c13ea077975129c3a030adecbd4bca620
Abel Santillan Rodriguez <diversia.abel@gmail.com>
2024-03-18 19:57:08 -0600

    Fix import statement in fno/__init__.py

--- fno/__init__.py
+++ fno/__init__.py
@@ -1,5 +1,5 @@
 from .afnonet import AdaptiveFourierNeuralOperator as AFNO
-from .afno import FourierBlock
+from .afno import FourierBlock  
 from .patch_embedding import PatchEmbedding
 from .mlp import MLP
 from .spectral_convolution import AdaptiveSpectralConvolution
